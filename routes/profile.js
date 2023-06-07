115fd2f155b7be3d6fb7f0ea4b0ff94b9f807606
AbhishekBansal <bansalabhishek7411@gmail.com>
2023-06-07 15:46:30 +0530

    debug

--- routes/profile.js
+++ routes/profile.js
@@ -6,100 +6,101 @@ const multer = require('multer');
 // const mongoose=require('mongoose');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.decoded.userName + ".jpg");
-    },
-  });
-  
-  const fileFilter= (req, file, cb) => {
-    if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  };
-  
-  const upload = multer({
-    storage: storage,
-    limits: {
-      fileSize: 1024 * 1024 * 6,
-    },
-    // fileFilter: fileFilter,
-  });
-  
-router.route("/add/image").patch(middleware.checkToken, upload.single("img"), async(req, res) => {
-    try{
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.decoded.userName + ".jpg");
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 6,
+  },
+  // fileFilter: fileFilter,
+});
+
+router.route("/add/image").patch(middleware.checkToken, upload.single("img"), async (req, res) => {
+  try {
     await Profile.findOneAndUpdate(
-        { userName: req.decoded.userName },
-        {$set: {img: req.file?.path}},
-        {new: true}
+      { userName: req.decoded.userName },
+      { $set: { img: req.file?.path } },
+      { new: true }
     );
     res.json("done");
-    }
-    catch(err){
-      console.log(err)
-    }
-    
+  }
+  catch (err) {
+    console.log(err)
+  }
+
 });
 
 router.route("/add").post(middleware.checkToken, (req, res) => {
-    const newProfile = Profile({
-        userName: req.decoded.userName,
-        name: req.body.name,
-        DOB: req.body.DOB,
-        titleLine: req.body.titleLine,
-        proffesion: req.body.proffesion,
-        about: req.body.proffesion
+  const newProfile = Profile({
+    userName: req.decoded.userName,
+    name: req.body.name,
+    DOB: req.body.DOB,
+    titleLine: req.body.titleLine,
+    proffesion: req.body.proffesion,
+    about: req.body.proffesion
+  });
+  newProfile.save().then((result) => {
+    return res.status(200).json("New Profile Created Succesfully");
+  }).catch((err) => {
+    return res.status(400).json({
+      err: err,
+      msg: "username already exist"
     });
-    newProfile.save().then((result) => {
-        return res.status(200).json("New Profile Created Succesfully");
-    }).catch((err) => {
-        return res.status(400).json({
-            err: err,
-            msg: "username already exist"
-        });
-    });
+  });
 })
 
-router.route("/update").post(middleware.checkToken, async(req, res)=>{
-    await Profile.findOneAndReplace(
-        {userName: req.decoded.userName},
-        {
-            proffesion: req.body.proffesion,
-            titleLine: req.body.titleLine,
-            about: req.body.about
-        },
-        {new: true}
-    ).then(replacedDoc => {
-        console.log(replacedDoc);
-        res.json("done");
-      })
-      .catch(error => {
-        console.error(error);
-      });
+router.route("/update").post(middleware.checkToken, async (req, res) => {
+  await Profile.findOneAndReplace(
+    { userName: req.decoded.userName },
+    {
+      proffesion: req.body.proffesion,
+      titleLine: req.body.titleLine,
+      about: req.body.about
+    },
+    { new: true }
+  ).then(replacedDoc => {
+    console.log(replacedDoc);
+    res.json("done");
+  })
+    .catch(error => {
+      console.error(error);
+    });
 });
 
 
 
 router.route("/checkProfile").get(middleware.checkToken, async (req, res) => {
-    try {
-      let profile=await Profile.findOne({userName: req.decoded.userName});
-      if(profile==null){
-        res.status(200).json({status: false});
-      }
-      else{
-        res.status(200).json({status: true});
-      }
-    } catch (e) {
-      console.log(e);
-      res.json("server error");
+  try {
+    let profile = await Profile.findOne({ userName: req.decoded.userName });
+    if (profile == null) {
+      res.status(200).json({ status: false });
     }
+    else {
+      res.status(200).json({ status: true });
+    }
+  } catch (e) {
+    console.log(e);
+    res.json("server error");
+  }
 });
+
 router.route("/anything").get((req, res) => {
-    res.json("done");
+  res.json("done");
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ const middleware = require("../middleware");
 
 
 router.route("/:username").get(middleware.checkToken, async (req, res) => {
+   try {
     const user = await User.findOne({ userName: req.params.username });
     if (user != null) {
         res.json({
@@ -19,10 +20,17 @@ router.route("/:username").get(middleware.checkToken, async (req, res) => {
             msg: "user not found"
         });
     }
+   } catch (error) {
+    console.log(error);
+    res.json({error: error});
+    
+   }
+    
 });
 
 router.route("/checkuserName/:userName").get(async (req, res) => {
-    const user = await User.findOne({ userName: req.params.userName });
+    try {
+        const user = await User.findOne({ userName: req.params.userName });
     if (user == null) {
         return res.json({
             msg: false
@@ -32,11 +40,18 @@ router.route("/checkuserName/:userName").get(async (req, res) => {
                 msg: true
             });
     }
+    } catch (error) {
+        res.json({error: error});
+        console.log(error);
+        
+    }
+    
 });
 
 
 router.route("/login").post(async (req, res) => {
-    const user = await User.findOne({ userName: req.body.userName });
+    try {
+        const user = await User.findOne({ userName: req.body.userName });
     if (user != null) {
         if ((user.userName === req.body.userName) && (user.password === req.body.password)) {
             let token = jwt.sign({ userName: req.body.userName }, config.key, { expiresIn: "24h" });
@@ -51,49 +66,74 @@ router.route("/login").post(async (req, res) => {
     } else {
         res.json("user not found");
     }
+        
+    } catch (error) {
+        res.json({error: error});
+        console.log(error);
+        
+    }
+    
 })
 
 router.route("/register").post((req, res) => {
-    console.log("inside the register");
 
-    var newUser = new User({
-        userName: req.body.userName,
-        password: req.body.password,
-        email: req.body.email
-    });
-    newUser.save().then((result) => {
-        let token = jwt.sign({ userName: req.body.userName }, config.key, { expiresIn: "24h" });
-        res.json({
-            token: token,
-            msg: "new user added"
-        })
-    }).catch((err) => {
-        res.json({
-            err: err
+    try {
+        var newUser = new User({
+            userName: req.body.userName,
+            password: req.body.password,
+            email: req.body.email
         });
-    });;
+        newUser.save().then((result) => {
+            let token = jwt.sign({ userName: req.body.userName }, config.key, { expiresIn: "24h" });
+            res.json({
+                token: token,
+                msg: "new user added"
+            })
+        }).catch((err) => {
+            res.json({
+                err: err
+            });
+        });
+        
+    } catch (error) {
+        res.json(error);
+        console.log(error);
+        
+    }
+
+    
 });
 
 router.route("/update/:username").patch(async (req, res) => {
 
-    var updated = await User.findOneAndUpdate(
-        { userName: req.params.username },
-        req.body,
-        {
-            new: true
+    try {
+
+        var updated = await User.findOneAndUpdate(
+            { userName: req.params.username },
+            req.body,
+            {
+                new: true
+            }
+        );
+        if (updated != null) {
+            updated.password = req.body.password;
+            res.json({
+                msg: "updated"
+            });
         }
-    );
-    if (updated != null) {
-        updated.password = req.body.password;
-        res.json({
-            msg: "updated"
-        });
+        else {
+            res.json({
+                msg: "user don't exist"
+            });
+        }
+        
+    } catch (error) {
+        res.json(error);
+        console.log(error);
+        
     }
-    else {
-        res.json({
-            msg: "user don't exist"
-        });
-    }
+
+    
 });
 
 router.route("/delete/:username").delete(async function (req, res) {
